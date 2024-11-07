6e0df5982e0b5a4ca5e0931880954195a3377abc
judewood <judewoodprofessional@gmail.com>
2024-11-07 14:49:09 +0000

    Added unary request streaming response example

--- greet/client/greet.go
+++ greet/client/greet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 
 	pb "github.com/judewood/gRPCSample/greet/proto"
@@ -16,4 +17,26 @@ func DoGreet(c pb.GreetServiceClient) {
 		log.Fatalf("failed to send greet request. Error %v", err)
 	}
 	log.Printf("response received %s", res.GetResult())
+}
+
+func DoGreetMany(c pb.GreetServiceClient) {
+	log.Println("doGreetMany was invoked")
+	req :=  pb.GreetRequest{
+		FirstName: "Freds",
+	}
+	stream, err := c.GreetMany(context.Background(), &req)
+	if err != nil {
+		log.Fatalf("failed to send greet many request. Error %v", err)
+	}
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			log.Println("End of stream")
+			break
+		}
+		if err != nil {
+			log.Fatalf("failed during stream. Error %v", err)
+		}
+		log.Printf("response received %s", msg.Result)
+	}
 }
