a42d4b760902415e313c684a658f500c68d6caef
judewood <judewoodprofessional@gmail.com>
2024-11-17 16:53:30 +0000

    Using vite generated react to avoid webpack issues

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -4,4 +4,4 @@ const ClientUrl string = "localhost:4444"
 const ServerUrl string = "0.0.0.0:4444"
 const TCP string = "tcp" //  transport protocol
 const UseSSL bool = true //if we are using evans cli or HTTP1 then set this false
-const NoProtoBuf = true
+const NoProtoBuf = false
